6d4160f612cef40efaa9e3e691c26d24fcc0fd35
Michael Wright <maiklpolkovnikov@gmail.com>
2023-11-25 15:43:55 +0300

    Working extracting routine

--- wdb.py
+++ wdb.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import zlib
 from Crypto.Cipher import ARC4
 from Crypto.Hash import MD5
 
@@ -22,11 +23,16 @@ def get_cipher():
     return RC4_Cipher
 
 class WdbFile:
-    def __init__(self, filename_in) -> None:
+    def __init__(self, filename_in, out_dir) -> None:
         self.file_in = open(filename_in, 'rb')
+        self.out_dir = out_dir
+        if self.out_dir == None:
+            self.out_dir = os.path.dirname(filename_in)
+
         self.__decrypt_header()
         self.__decrypt_footer()
         self.__find_files()
+        self.__extract()
 
     def __del__(self):
         self.file_in.close()
@@ -55,7 +61,7 @@ class WdbFile:
         print("SW: %s" % self.wdb_sw)
     
     def __decrypt_footer(self):
-        self.file_in.seek(-4, 2) # от конца 4-ый байт
+        self.file_in.seek(-4, 2) # the 4th byte from the end
         ending_byte = int.from_bytes(self.file_in.read(1), 'little')
         #print(hex(ending_byte))
 
@@ -67,58 +73,67 @@ class WdbFile:
         #print("Footer ecrypted:")
         #print_bytes(footer_decrypted)
 
-        #!
         self.num_of_files = int(footer_decrypted[0])
         print("Number of files: %d" % self.num_of_files)
 
-        #self.files_offset = footer_decrypted[4]
-
         self.files_offset = []
         for i in range(0, self.num_of_files):
-            #self.file_in.seek(-footer_decrypted[4] - i*4, 2)
             file_header_len = footer_decrypted[4+i*4:4+i*4+4]
             self.files_offset.append( int.from_bytes(file_header_len, 'little') )
 
 
     def __find_files(self):
+        self.files = []
         for i in range(0, self.num_of_files):
-            print()
             self.file_in.seek(self.files_offset[i], 0)
             file_header_len = self.file_in.read(4)[0]
-            print("File offset: %s" % hex(self.files_offset[i]))
-            print("File header length: %s" % hex(file_header_len))
+            #print("File offset: %s" % hex(self.files_offset[i]))
+            #print("File header length: %s" % hex(file_header_len))
 
             some = self.file_in.read(4)
-            print_bytes(some)
+            #print_bytes(some)
             file_header = self.file_in.read(file_header_len)
             file_header_decrypted = get_cipher().decrypt(file_header)
             #print_bytes(file_header_decrypted)
 
             header1byte = file_header_decrypted[0:4]
             len_of_filename = file_header_decrypted[4]
-            header3byte = file_header_decrypted[8:12]
+            filesize = int.from_bytes(file_header_decrypted[8:12], 'little')
             header4byte = file_header_decrypted[12:16]
 
-            print("header1byte: ", end="")
-            print_bytes(header1byte)
+            #print("header1byte: ", end="")
+            #print_bytes(header1byte)
 
             filename = file_header_decrypted[16:16+len_of_filename].decode('ascii')
-            print("File name: %s" % filename)
+            print("File #%d: '%s', size = %d (zlib compressed)" % (i, filename, filesize))      
+            #print("header4byte: ", end="")
+            #print_bytes(header4byte)
+            self.files.append({
+                'name': filename,
+                'size': filesize,
+                'offset': self.files_offset[i],
+                'header_length': file_header_len
+            })
 
-            print("header3byte: ", end="")
-            print_bytes(header3byte)            
-            print("header4byte: ", end="")
-            print_bytes(header4byte)
+    def __extract(self):
+        for file in self.files:
+            self.file_in.seek(file['offset']+file['header_length']+4+4, 0)
+            file_out_name = self.out_dir + os.sep + file['name'].split('\\')[1]
+            print("Extracting '%s'..." % file['name'].split('\\')[1])
 
-            
+            raw = self.file_in.read(file['size'])
+            decrypted = get_cipher().decrypt(raw)
+
+            f_out = open(file_out_name, "wb")
+            decompressed = zlib.decompress(decrypted)
+            f_out.write(decompressed)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                         prog = 'wdb.py',
                         description = 'Extract WDB file for old LG phones')
-    parser.add_argument('-d', help='Decrypt KDZ file', action='store_true')
-    parser.add_argument('-e', help='Encrypt CAB file', action='store_true')
-    parser.add_argument('-o', help='Output filename. By default the converted file is saved in the same folder.', type=str)
+    parser.add_argument('-o', help='Output directory. By default files are extracted in the same folder', type=str)
     parser.add_argument('filename', help='Path to the file', type=str)
     args = parser.parse_args()
 
@@ -126,5 +141,4 @@ if __name__ == '__main__':
         print("File doesn't exist")
         exit()
 
-    if args.d == True:
-        WdbFile(args.filename)
+    WdbFile(args.filename, args.o)
